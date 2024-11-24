16a602408defdd65a915eb6f660dbe5fbc852cac
DESKTOP-6IFHR12\asrar <shahasrar957@gmail.com>
2024-11-24 15:51:18 +0530

    updated dashboard ui

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const ejs = require('ejs');
 const connectDB = require('./backend/config/db');
 const eventRoutes = require('./backend/routes/eventRoutes');
-
+const path = require("path");
 const app = express();
-
+app.set('views', path.join(__dirname, '/backend/views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 // Connect to MongoDB
 connectDB();
 
@@ -14,6 +17,11 @@ app.use(express.json());
 
 // Routes
 app.use('/api/events', eventRoutes);
+app.get('/',(req,res)=>
+{
+    res.render('dashboard');
+
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
